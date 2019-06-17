5ee451018f65eebe118b8e62356e451e41e34745
Jozsef Hegedus <jhegedus@pm.me>
2019-06-17 07:50:04 +0300

    webpack-et kicsit megtisztitottuk, packag.json-t is, bumpoltunk par verziot, es az index-bundle.js -t nem irtuk at index.js -re : http://localhost:8043/node/generated.js/index-bundle.js => ezt kell atnevezni

---
+++ node/index.js
@@ -0,0 +1,5 @@
+window.React = require('react');
+window.ReactDOM = require('react-dom');
+
+import {render} from 'react-dom';
+
--- node/webpack.config.js
+++ node/webpack.config.js
@@ -2,44 +2,24 @@
 var path = require('path');
 var root = './'
 var webpack = require('webpack');
-var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 module.exports = {
     devtool: "source-map",
     entry: {
-        index: './bundles/index.js',
+        index: './index.js',
     },
     output: {
         path: __dirname + '/generated.js/',
         publicPath: "/assets/",
-        filename: '[name]-bundle.js'
+        filename: 'bundle.js'
     },
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new CommonsChunkPlugin({
-            name: "index"
-        }),
-	new webpack.DefinePlugin({
-	    'process.env': {
-		NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-	    },
-	})
-    ],
     module: {
         rules: [
             { test: /\.js$/,
                 enforce: "pre",
                 exclude: /node_modules/,
                 loader: "source-map-loader" }
-        ],
-        loaders:
-	[
-	    {
-		    test: /\.js[x]?$/,
-		    include: path.resolve(__dirname, './'),
-		    exclude: /node_modules/, loader: 'babel-loader'
-	    }
-	]
+        ]
     }
 };
 
