a1bf37187a77bce33a315ff7dd24a4bd369b8d1f
Jozsef Hegedus <jhegedus@pm.me>
2019-07-06 07:12:26 +0300

    Placeholder for calculating the sum of the Thievery Numbers on the server.

--- layer_V_JS_client/src/main/scala/app/client/ui/components/mainPageComponents/components/cacheTestMainPageComp/AddTheThieveryNumbersUsingTheServer.scala
+++ layer_V_JS_client/src/main/scala/app/client/ui/components/mainPageComponents/components/cacheTestMainPageComp/AddTheThieveryNumbersUsingTheServer.scala
@@ -1,25 +1,31 @@
 package app
   .client.ui.components.mainPageComponents.components.cacheTestMainPageComp
 
-import japgolly.scalajs.react._
+import app.client.ui.caching.viewCache.SumIntViewCache
+import app.client.ui.components.mainPageComponents.components.cacheTestMainPageComp
+import app.shared.rest.views.viewsForDevelopingTheViewFramework.SumIntView_HolderObject
+import app.shared.rest.views.viewsForDevelopingTheViewFramework.SumIntView_HolderObject.SumIntView_Par
+import japgolly.scalajs.react.{CtorType, _}
+import japgolly.scalajs.react.component.Scala.Component
 import japgolly.scalajs.react.vdom.html_<^.{<, VdomElement, _}
 import org.scalajs.dom.html.Input
 
 object AddTheThieveryNumbersUsingTheServer {
+
   type State = TheThieveryNumber
-  val TheCorporation = ScalaComponent
-    .builder[Unit]( "Example" )
-    .initialState( TheThieveryNumber( 0.38, 0.45 ) )
-    .renderBackend[Backend] // ← Use Backend class and backend.render
-    .build
+
+  val TheCorporation
+      : Component[Unit,
+                  TheThieveryNumber,
+                  ThieveryUndergroundBackend,
+                  CtorType.Nullary] = ScalaComponent.builder[Unit]( "Example" )
+      .initialState( TheThieveryNumber( 0.38, 0.45 ) )
+      .renderBackend[ThieveryUndergroundBackend] // ← Use Backend class and backend.render
+      .build
 
   case class TheThieveryNumber(firstNumber: Double, secondNumber: Double ) {
 
-    def onChangeFirstNumber(
-        bs: BackendScope[Unit, State]
-      )(
-        e: ReactEventFromInput
-      ) = {
+    def onChangeFirstNumber( bs: BackendScope[Unit, State] )( e: ReactEventFromInput ) = {
       val event: _root_.japgolly.scalajs.react.ReactEventFromInput = e
       println( event )
       val target:   Input = event.target
@@ -27,53 +33,49 @@ object AddTheThieveryNumbersUsingTheServer {
       bs.modState( s => s.copy( firstNumber = newValue ) )
     }
 
-    def onChangeSecondNumber(
-        bs: BackendScope[Unit, State]
-      )(
-        e: ReactEventFromInput
-      ) = {
-      val event: _root_.japgolly.scalajs.react.ReactEventFromInput = e
+    def onChangeSecondNumber( bs: BackendScope[Unit, State] )( e: ReactEventFromInput ) = { val event: _root_.japgolly.scalajs.react.ReactEventFromInput = e
       println( event )
       val target:   Input = event.target
       val newValue: Double = target.valueAsNumber
-      bs.modState( s => s.copy( secondNumber = newValue ) )
-    }
+      bs.modState( s => s.copy( secondNumber = newValue ) )}}
 
-  }
+  def getLineBreaks(i: Int ) = TagMod( List.fill( i )( <.br ).toIterator.toTraversable.toVdomArray )
 
-  class Backend(bs: BackendScope[Unit, State] ) {
+  class ThieveryUndergroundBackend(bs: BackendScope[Unit, State] ) {
 
-    def render(
-        s: State
-      ): VdomElement = // ← Accept props, state and/or propsChildren as argument
+    def render( s: State ): VdomElement =
       <.div(
         <.hr,
-        <.h3(
-          "Itt van a Thievery Number osszeado alkalmazas (USING THE SERVER)!"
-        ),
-        <.br,
-        <.br,
-        <.br,
-        <.br,
-        <.br,
-        <.div(
-          s"${s.firstNumber} ${s.secondNumber} The thievery number, the corporation."
-        ),
-        <.div(
-          s"The sum of the thievery numbers is : ${s.firstNumber + s.secondNumber}"
-        ),
-        <.input.number( ^.onChange ==> s.onChangeFirstNumber( bs ),
-                       ^.value := s.firstNumber ),
-        <.input.number( ^.onChange ==> s.onChangeSecondNumber( bs ),
-                       ^.value := s.secondNumber ),
-        <.br,
-        <.br,
-        <.hr,
-        "start tagmod 10 BR",
-        TagMod(List.fill(10)(<.br).toIterator.toTraversable.toVdomArray),
-        "end tagmond 10 BR",
-        <.br
-      )
 
+        <.h3( "Itt van a Thievery Number osszeado alkalmazas (USING THE SERVER)!" ),
+
+        getLineBreaks(5),
+
+        <.div(  s"${s.firstNumber} " +
+                s"${s.secondNumber} " +
+                s"The thievery number, the corporation." ),
+
+        <.div( s"The sum of the thievery numbers is : " +
+               s"${s.firstNumber + s.secondNumber}" ),
+
+        <.input.number( ^.onChange ==> s.onChangeFirstNumber( bs ),
+                        ^.value := s.firstNumber ),
+        <.input.number( ^.onChange ==> s.onChangeSecondNumber( bs ),
+                        ^.value := s.secondNumber ),
+
+        getLineBreaks(5),
+
+        "Here is the sum of the Thievery Numbers (as Integers), calculated on the server:",
+
+        <.br,
+
+        {
+          val params: SumIntView_Par = SumIntView_Par( 38, 45 )
+          val res: Option[SumIntView_HolderObject.SumIntView_Res] =
+            SumIntViewCache.getSumIntView( params )
+          res.toString()
+        }
+
+      )
   }
 }
