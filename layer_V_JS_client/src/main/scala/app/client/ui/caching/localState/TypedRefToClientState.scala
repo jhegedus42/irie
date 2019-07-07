8bb8b5e5b129514684683d91a9087165111b61af
Jozsef Hegedus <jhegedus@pm.me>
2019-07-08 02:16:27 +0300

    client state containing map has been written

---
+++ layer_V_JS_client/src/main/scala/app/client/ui/caching/localState/TypedRefToClientState.scala
@@ -0,0 +1,67 @@
+package app.client.ui.caching.localState
+
+import java.util
+
+import app.client.ui.components.mainPageComponents.components.cacheTestMainPageComp.AddTheThieveryNumbersUsingTheServer.TheThieveryNumber
+import app.shared.data.model.Entity.Entity
+import app.shared.data.ref.TypedRef
+
+import scala.reflect.ClassTag
+import scala.collection.immutable.HashMap
+
+trait ClientStateEntity extends Entity
+
+case class TypedRefToClientState[CSE <: ClientStateEntity : ClassTag](tr: TypedRef[CSE])
+
+case class ClientStateVal[CSE <: ClientStateEntity : ClassTag](typedRefToClientState: TypedRefToClientState[CSE],
+                                                               cse: ClientStateEntity)
+
+object TypedRefToClientState {
+  def makeNew[CSE <: ClientStateEntity : ClassTag]: TypedRefToClientState[CSE] = {
+    val tr: TypedRef[CSE] = TypedRef.make[CSE]
+    val trcs: TypedRefToClientState[CSE] = TypedRefToClientState(tr)
+    trcs
+  }
+}
+
+
+
+trait ClientSideStateContainingMap[CSE <: ClientStateEntity] {
+
+  sealed trait DidUpdateWork
+  object Success extends DidUpdateWork
+  object Failure extends DidUpdateWork
+
+  private var map: HashMap[TypedRefToClientState[CSE], ClientStateVal[CSE]] =
+    new HashMap[TypedRefToClientState[CSE], ClientStateVal[CSE]]()
+
+  def addNewValue(clientStateEntity: CSE)(implicit classTag: ClassTag[CSE]) = {
+      val tr: TypedRefToClientState[CSE] = TypedRefToClientState.makeNew[CSE]
+      val clientStateVal: ClientStateVal[CSE] =ClientStateVal(tr,clientStateEntity)
+      val keyValue: (TypedRefToClientState[CSE], ClientStateVal[CSE]) = (tr -> clientStateVal)
+      val newMap = map + keyValue
+      map=newMap
+  }
+
+  def update(clientStateVal: ClientStateVal[CSE]):DidUpdateWork  ={
+    if(map.contains(clientStateVal.typedRefToClientState)){
+      val key: TypedRefToClientState[CSE] = clientStateVal.typedRefToClientState
+      val keyValue: (TypedRefToClientState[CSE], ClientStateVal[CSE]) = (key -> clientStateVal)
+      val newMap = map + keyValue
+      map=newMap
+      return Success
+    } else return Failure
+
+  }
+
+}
+
+object ClientSideStateContainer {
+
+}
+
+object ClientSideModel {
+
+
+}
+
