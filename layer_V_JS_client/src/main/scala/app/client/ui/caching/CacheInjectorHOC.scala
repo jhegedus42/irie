aa91611dd549f99126b1de04ea23f1c543cfe7f9
Jozsef Hegedus <jhegedus@pm.me>
2019-07-24 02:45:56 +0300

    Implementing a proper ViewCache - "longer term project" - few days ... if things go "well" :) .
    Introducing wrapper classes for making AJAX View requests more
    "explicit" / "type-safe", especially in the client methods,
    such as "getViewFromServer" and alike. So that the types tell
    what the data means.
    
    -----------------------------------------------------------
    Monday, 22 July 2019 3.54.32 :
    
    Incorporating these wrapper classes into the functions in
    `object REST_ForView`, now those functions seem to compile.
    -----------------------------------------------------------
    
    Monday, 22 July 2019 4.01.52 :
    
    Adding some multi line TODO comment.
    
    -----------------------------------------------------------
    
    Monday, 22 July 2019 16.33.47 :
    
    Getting rid of some type errors.
    Code still does not compile.
    Still going towards the "right" View cache implementation,
    instead of the one where the " `Map[ , ]` `" was just a
    simple dummy `Option[ ]``.
    
    ------------------------------------------------------------
    
    Monday, 22 July 2019 23.45.57 :
    
    The code compiles now, but it does not "work":
    we should replace calls to this:
    `app.client.ui.caching.viewCache.SumIntViewCache#getSumIntView`
    with this :
    `app.client.ui.caching.CacheInterface#readView` .
    
    The first one does not work anymore and the second one is not implemented yet.
    
    ------------------------------------------------------------
    
    Tuesday, 23 July 2019 0.00.01 :
    
    Code compiles, now working on :
    `app.client.ui.caching.viewCache.ViewCache#getViewCacheState`
    to be like `readEntity` in `EntityCache`.
    
    ------------------------------------------------------------
    
    Adding todo-s - Tuesday, 23 July 2019 0.03.03
    
    ------------------------------------------------------------
    
    Tuesday, 23 July 2019 4.50.58 :
    
    scalafmt question commit
    
    ------------------------------------------------------------
    Tuesday, 23 July 2019 5.26.39
    
    Simplifying the code ... it does not compile, has some
    problems with the reRenderTriggerer in the
    `CacheInjectorHOC`
    
    ------------------------------------------------------------
    Wednesday, 24 July 2019 0.15.33
    
    Code compiles now. But we must still implement
    `getViewCacheState` before we can try running the
    "new ViewCache".
    
    ------------------------------------------------------------
    Wednesday, 24 July 2019 2.44.52
    ˆ
    Making the `CacheInjectorHOC` class a bit tidier.

--- layer_V_JS_client/src/main/scala/app/client/ui/caching/CacheInjectorHOC.scala
+++ layer_V_JS_client/src/main/scala/app/client/ui/caching/CacheInjectorHOC.scala
@@ -11,53 +11,25 @@ import japgolly.scalajs.react.component.Scala.Component
 import japgolly.scalajs.react.component.builder.Lifecycle
 
 class CacheInjectorHOC(
-    toBeWrapped: Component[CacheTest_RootComp_Props,
-                           Unit,
-                           NotWrapped_CacheTestRootComp_Backend,
-                           CtorType.Props]) {
+                        toBeWrapped: Component[
+                          CacheTest_RootComp_Props,
+                          Unit,
+                          NotWrapped_CacheTestRootComp_Backend,
+                          CtorType.Props ]
+                      ) {
 
   lazy val wrapperConstructor =
     ScalaComponent
-      .builder[CacheTest_RootComp_Props]( "Wrapper" )
+      .builder[CacheTest_RootComp_Props]("Wrapper")
       .renderBackend[WrapperBackend]
-      .componentWillMount( $ => {
-        println(
-          "we are creating the callback that will be " +
-            "executed when the wrapper component will mount"
-        )
-        Callback {
-
-          println( "--------------- Our CALLBACK HAS BEEN CALLED !!!!" )
-          val h: Lifecycle.ComponentWillMount[CacheTest_RootComp_Props,
-                                              Unit,
-                                              WrapperBackend] = $
-
-          def f(): Unit = {
-            val s = $.setState( Unit )
-            println(
-              s"we will run the callback that will set the state of the Wrapper Component " +
-                s"to Unit, hopefully this should cause a re-render"
-            )
-            s.runNow()
-          }
-
-          val reRenderTriggerer: ReRenderTriggerer = ReRenderTriggerer( f )
-
-          println(
-            "we are now just about to mount the Wrapper component (as the direct child of the Router)," +
-              "kind of as a bridge between the Router and the 'real deal', we need this bridge so that we can" +
-              "trigger a re-render when the cache has been updated / freshed / 'filled-up' "
-          )
-
-          ReRenderer
-            .setTriggerer( reRenderTriggerer )
-        }
-      } )
+      .componentWillMount( $ => Callback { ReRenderer.
+        setTriggerer(ReRenderTriggerer(() =>{ $.setState(Unit).runNow()}))})
       .build
 
-  class WrapperBackend($ : BackendScope[CacheTest_RootComp_Props, Unit] ) {
-    def render(props: CacheTest_RootComp_Props ) = {
-      <.div( toBeWrapped( props ) )
+  class WrapperBackend($: BackendScope[CacheTest_RootComp_Props, Unit]) {
+
+    def render(props: CacheTest_RootComp_Props) = {
+      <.div(toBeWrapped(props))
     }
   }
 
