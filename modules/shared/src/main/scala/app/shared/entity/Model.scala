4830f4127be3e32cd99c491dcc5c80adb5efd993
Jozsef Hegedus <jhegedus@pm.me>
2019-08-10 04:05:07 +0300

    Still "simplifying" the code.
    Fixed a few compiler errors.
    
    Adding a few todo-s.

--- modules/shared/src/main/scala/app/shared/entity/Model.scala
+++ modules/shared/src/main/scala/app/shared/entity/Model.scala
@@ -2,14 +2,37 @@ package app.shared.entity
 
 import app.shared.entity.Entity.Entity
 
-case class User( name: String ) extends Entity[User]
+case class User( login: String, password : String, isAdmin: Boolean ) extends Entity[User]
 
-// TODO CRUD PAGE FOR THIS ^ :
-// TODO-NOW - create - TEST DATA : 3 USERS, Alice, Bob, Cecile
-// todo 1 - list them ordered by their name in the client
-// todo 2 - create a new user (create a React Page for This : Name, Save, Cancel)
-// todo 3 - change a user's name (create a React Page for This, NameField, Save, Cancel)
+// TODO CRUD PAGE FOR admin interface:
+// TODO-NOW - create - TEST DATA : 3 USERS, Alice, Bob, Cecile, + admin (user)
+//     => admin is a user
+//
+// todo next : admin can do the following (we don't care about authenticating yet)
+//  there is one single admin user
+//  and that user has an "admin" interface
+//
+// todo 1 - list them (users) ordered by their name in the client (in the admin interface)
+// todo 2 - create a new user (create a React Page for This : login, password, Save, Cancel)
+//     => 2.0
+//        => create a TypeRef/TypeRefVal for an entity
+//     => 2.1
+//        => this will set the new user's user version to ZERO
+//     => 2.2
+//        => deny this requestion if user with this login exist already
+//
+//
+// todo 3 - edit user (same react page as for todo-2 )
+//     => this will increase the user's version by one
 // todo 4 - delete a user (Are you sure ? Dialog : Yes / Cancel)
+//     => this will increase the user's version by one
+// todo 5 - make sure that :
+//     in todos 3,4 :
+//       if the user's version of the admin does not match the one
+//       in the persistent actor, then deny the update request
+//
+
+
 //  implement this deletion as a "deleted" flag, next to the "version" flag
 
 /**
