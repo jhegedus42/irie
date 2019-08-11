e66f9a3825aa013925e38170bcd9353c0e9d9a47
Jozsef Hegedus <jhegedus@pm.me>
2019-08-11 20:21:16 +0300

    Wrote a "todo-now" list.

--- modules/server/src/main/scala/app/server/Main.scala
+++ modules/server/src/main/scala/app/server/Main.scala
@@ -6,7 +6,7 @@ object Main extends App {
 
   // todo-now - make the app "start"
   // steps:
-  //  - step 1 : make sbt to start the "App"
+  //  - step 1 : make sbt to start the "App" (this object here, print "hello world")
   //  - step 2 : start the "server" on localhost
   //  - step 3 : serve simple static content (a simple .txt file from the www directory)
   //  - step 4 : serve "the react page"
