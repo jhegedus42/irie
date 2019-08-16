84d1a3fcfbf06e5f138bd34e7912dd6005ffa540
Jozsef Hegedus <jhegedus@pm.me>
2019-08-16 14:13:06 +0300

    => Wednesday, 14 August 2019 22.47.14
       Making the entity related types of the code more
       meaningful.
    
    => Wednesday, 14 August 2019 23.23.02
    
       Adding simple corrections, and some simple things
       for tests and pretty printing and embedding
       compilation time into the final binaries (using macros).
    
    => Wednesday, 14 August 2019 23.29.09
    
       A bit more refining of how the test results are printed, etc...
    
    => Thursday, 15 August 2019 7.11.22
    
       Lot of changes, towards creating a persistence layer into which
       we can insert a simple entity.
    
    => Friday, 16 August 2019 2.50.17
    
       Working towards create a new entity from a value and
       storing it in the "database" (journal + immutable map in a
       persistent actor).
    
    => Friday, 16 August 2019 4.18.38
       Writing a test for creating and inserting a new Entity
       into the Persistent Actor's Application State Map.
    
       Next step: make this test pass. It is in PersistenceModuleTest,
       it is called "init".
    
    => Friday, 16 August 2019 5.42.53
    
       Writing some more todo's and specifying how the
       implementations of some functions will look.
       ("sort of like pseudo code")
    
    => Friday, 16 August 2019 6.12.07
    
       Added lot of todo-s and sketches for the create/insert
       entity related functions/logic.
    
       Now, the "plan" is ready, "only" the "simple work" remains
       to be done :).
    
    => Friday, 16 August 2019 13.59.34
    
       Completed a todo, in Application State Map.
    
    => Friday, 16 August 2019 14.05.22
    
       Completed todo in : `receiveCommand`.
    
    => Friday, 16 August 2019 14.12.15
    
       Working on insertEntity in PersActorWrapper, that
       is still under progress.

--- modules/server/src/main/scala/app/server/httpServer/persistence/persistentActor/PersActorWrapper.scala
+++ modules/server/src/main/scala/app/server/httpServer/persistence/persistentActor/PersActorWrapper.scala
@@ -1,12 +1,9 @@
 package app.server.httpServer.persistence.persistentActor
 
 import akka.actor.ActorRef
-import app.server.httpServer.persistence.persistentActor.PersistentActorCommands.{
-  GetAllStateCommand,
-  InsertNewEntityCommand
-}
+import app.server.httpServer.persistence.persistentActor.PersistentActorCommands.{GetAllStateCommand, InsertNewEntityCommand}
 import app.server.httpServer.persistence.persistentActor.Responses.InsertNewEntityCommandResponse
-import app.server.httpServer.persistence.state.{ ApplicationStateEntry, ApplicationStateMap }
+import app.server.httpServer.persistence.state.{ApplicationStateEntry, ApplicationStateMap, EntityAsJSON, UntypedRef}
 import app.shared.dataModel.value.EntityValue
 import app.shared.dataModel.value.refs.Entity
 
@@ -42,16 +39,24 @@ private[persistence] case class PersActorWrapper( val actor: ActorRef ) {
     *         (in a StateChange object, first element is the state before,
     *         second one is the state after the insertion).
     */
-  def insertEntity[E <: EntityValue[E]](
-      entityToInsert: Entity[E]
-  )( implicit executionContextExecutor: ExecutionContextExecutor ): Future[StateChange] = {
-    // todo-later we should check that the entity's version number is zero, i.e.
+  def insertEntity[E <: EntityValue[E]]( entityToInsert: Entity[E] )(
+    implicit executionContextExecutor: ExecutionContextExecutor ):
+      Future[StateChange] = {
+
+    // todo-later
+    //  we should check that the entity's version number is zero, i.e.
     //  we are dealing with an entity which has not been inserted into the
     //  ApplicationStateMap
 
-    val newEntry: ApplicationStateEntry = ???
-    // todo-now-4 insertEntity in PersActorWrapper
-    //  convert entity `E` into `ApplicationStateEntry`
+    val newEntry: ApplicationStateEntry = {
+       val utr:UntypedRef = ???
+       val entityAsJSON:EntityAsJSON= ???
+
+       // todo-now-4  ^^^ -- fill in ???-s
+
+       val applicationStateEntry=ApplicationStateEntry(utr, entityAsJSON)
+       applicationStateEntry
+    }
 
     val res: Future[InsertNewEntityCommandResponse] =
       ask( actor, InsertNewEntityCommand( newEntry ) )(
@@ -59,6 +64,7 @@ private[persistence] case class PersActorWrapper( val actor: ActorRef ) {
       ).mapTo[InsertNewEntityCommandResponse]
 
     res.map( x => x.stateChange )
+
   }
 
 }
