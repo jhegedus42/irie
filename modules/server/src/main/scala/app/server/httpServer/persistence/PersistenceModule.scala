775b615ca37d6bbd812a289be732cf77090361a5
Jozsef Hegedus <jhegedus@pm.me>
2019-08-18 01:27:00 +0300

    Friday, 16 August 2019 21.50.18:
    InsertNewEntityCommand in PersistenceModule
    
    Does not seem to store the change "right away".
    
    Maybe I need to issue a GetState Command, and see if that has
    worked.
    
    => Saturday, 17 August 2019 1.55.12:
       Insert and store new entity seems to work.
    
       Next step: getEntity + routes for both insert + get.
    
    => Saturday, 17 August 2019 2.13.16:
       Client side synchronous testing works.
    
    => Sunday, 18 August 2019 1.10.33 :
       getEntity implementation is in progress...
    
    => Sunday, 18 August 2019 1.24.07 :
       app.server.httpServer.persistence.PersistenceModule#getEntity is
       "ready", however there are two related ???-s that need to be filled
       in. Hopefully in the coming few commits they will be worked
       out as well.

--- modules/server/src/main/scala/app/server/httpServer/persistence/PersistenceModule.scala
+++ modules/server/src/main/scala/app/server/httpServer/persistence/PersistenceModule.scala
@@ -1,8 +1,16 @@
 package app.server.httpServer.persistence
 
 import akka.actor.{ActorRef, ActorSystem}
-import app.server.httpServer.persistence.persistentActor.state.{ApplicationStateMapEntry, UntypedRef}
-import app.server.httpServer.persistence.persistentActor.{AppPersistentActor, PersistentActorWrapper, Responses, StateChange}
+import app.server.httpServer.persistence.persistentActor.state.{
+  ApplicationStateMapEntry,
+  UntypedRef
+}
+import app.server.httpServer.persistence.persistentActor.{
+  AppPersistentActor,
+  PersistentActorWrapper,
+  Responses,
+  StateChange
+}
 import app.shared.dataModel.value.EntityValue
 import app.shared.dataModel.value.asString.EntityAsJSON
 import app.shared.dataModel.value.refs.{Entity, RefToEntity}
@@ -11,21 +19,21 @@ import io.circe.Encoder
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.reflect.ClassTag
 
-private[httpServer] case class PersistenceModule(actorSystem: ActorSystem) {
+private[httpServer] case class PersistenceModule( actorSystem: ActorSystem ) {
 
   implicit lazy val executionContext: ExecutionContextExecutor =
     actorSystem.dispatcher
 
   private val persistentActorWrapper: PersistentActorWrapper = {
     val peristentActor: ActorRef =
-      AppPersistentActor.getActor("one_and_only_parsistent_actor")
-    PersistentActorWrapper(peristentActor)
+      AppPersistentActor.getActor( "one_and_only_parsistent_actor" )
+    PersistentActorWrapper( peristentActor )
   }
 
   def getEntity[V <: EntityValue[V]](
 //                                      ref: RefToEntity[V]
-                                        untypedRef: UntypedRef
-                                    ): Future[Entity[V]] = {
+      untypedRef: UntypedRef
+  ): Future[Entity[V]] = {
 
     val eventualGetStateResult: Future[Responses.GetStateResult] =
       persistentActorWrapper.getState
@@ -33,53 +41,57 @@ private[httpServer] case class PersistenceModule(actorSystem: ActorSystem) {
 //    val untypedRef: UntypedRef = UntypedRef.makeFromRefToEntity(ref)
 
     val r1: Future[Option[ApplicationStateMapEntry]] =
-      eventualGetStateResult.map(r => r.state.map.get(untypedRef))
+      eventualGetStateResult.map( r => r.state.map.get( untypedRef ) )
 
-    val r2: Future[ApplicationStateMapEntry] = r1.flatMap((x: Option[ApplicationStateMapEntry]) => {
-      x match {
-        case Some(value) => Future.successful(value)
-        case None => Future.failed(
-          new Exception(
-            "val r2 in PersistenceModule.getEntity has failed. " +
-            "Please see the source code for details.")
-        )
-      }
-    })
+    val r2: Future[ApplicationStateMapEntry] =
+      r1.flatMap( (x: Option[ApplicationStateMapEntry]) => {
+        x match {
+          case Some( value ) => Future.successful( value )
+          case None =>
+            Future.failed(
+              new Exception(
+                "val r2 in PersistenceModule.getEntity has failed. " +
+                  "Please see the source code for details."
+              )
+            )
+        }
+      } )
 
-    val r3: Future[EntityAsJSON] = r2.map((x: ApplicationStateMapEntry) =>{
+    val r3: Future[EntityAsJSON] = r2.map( (x: ApplicationStateMapEntry) => {
       val json: EntityAsJSON = x.entityAsString.entityAsJSON
       json
-    })
+    } )
 
-    val r4: Future[Option[Entity[V]]] = r3.map( EntityAsJSON.getEntity[V](_))
+    val r4: Future[Option[Entity[V]]] = r3.map( EntityAsJSON.getEntity[V]( _ ) )
 
-    val r5: Future[Entity[V]] =  r4.flatMap((x: Option[Entity[V]]) =>{
+    val r5: Future[Entity[V]] = r4.flatMap( (x: Option[Entity[V]]) => {
       x match {
-        case Some(value) => Future.successful(value)
-        case None => Future.failed(
-          new Exception(
-            "val r5 in PersistenceModule.getEntity has failed. " +
-            "Please see the source code for details.")
-        )
+        case Some( value ) => Future.successful( value )
+        case None =>
+          Future.failed(
+            new Exception(
+              "val r5 in PersistenceModule.getEntity has failed. " +
+                "Please see the source code for details."
+            )
+          )
       }
 
-
-    })
+    } )
     r5
   }
 
-  def createAndStoreNewEntity[V <: EntityValue[V] : ClassTag](
-                                                               value: V
-                                                             )(
-                                                               implicit encoder: Encoder[Entity[V]]
-                                                             ): Future[(StateChange, Entity[V])] = {
+  def createAndStoreNewEntity[V <: EntityValue[V]: ClassTag](
+      value: V
+  )(
+      implicit encoder: Encoder[Entity[V]]
+  ): Future[( StateChange, Entity[V] )] = {
 
-    val entity: Entity[V] = Entity.makeFromValue(value)
+    val entity: Entity[V] = Entity.makeFromValue( value )
 
     val res: Future[StateChange] =
-      persistentActorWrapper.insertEntity[V](entity)
+      persistentActorWrapper.insertEntity[V]( entity )
 
-    res.map(x => (x, entity))
+    res.map( x => ( x, entity ) )
   }
 
 }
