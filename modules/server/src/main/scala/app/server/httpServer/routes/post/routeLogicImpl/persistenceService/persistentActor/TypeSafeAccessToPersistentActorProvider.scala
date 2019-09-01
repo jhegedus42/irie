d4d3444a238c398c6efefbf01754c6f69111baa8
Jozsef Hegedus <jhegedus@pm.me>
2019-09-01 03:19:57 +0300

    -------------------------------------------------- Saturday, 31 August 2019 2.01.51 :
    Code compiles.
    
    Introduced the "PersistentActorWhisperer" case class-t.
    
    After implementing the "Get" part "there", then we
    might be able to start to see the light at the end of the
    Get tunnel. :)
    
    --------------------------------------------------
    
    Sunday, 1 September 2019 0.32.55:
    
    Added basic "foundation" for OCC.
    
    --------------------------------------------------
    Sunday, 1 September 2019 0.51.54:
    
    Making the names a bit more explicit.
    
    --------------------------------------------------
    
    Sunday, 1 September 2019 0.53.17:
    
    Setting priorities for todo-s.
    
    --------------------------------------------------
    Sunday, 1 September 2019 3.19.50:
    
    Adding dummy implementation for a get Entity unit test
    --------------------------------------------------

--- modules/server/src/main/scala/app/server/httpServer/routes/post/routeLogicImpl/persistenceService/persistentActor/TypeSafeAccessToPersistentActorProvider.scala
+++ modules/server/src/main/scala/app/server/httpServer/routes/post/routeLogicImpl/persistenceService/persistentActor/TypeSafeAccessToPersistentActorProvider.scala
@@ -4,7 +4,9 @@ import akka.actor.ActorRef
 import app.server.httpServer.routes.post.routeLogicImpl.persistenceService.persistentActor.logic.PersistentActorImpl
 import app.shared.entity.Entity
 import app.shared.entity.entityValue.EntityValue
+import app.shared.entity.entityValue.values.User
 import app.shared.entity.refs.RefToEntityWithVersion
+import app.shared.initialization.testing.TestUsers
 import io.circe.Decoder
 
 import scala.concurrent.Future
@@ -25,6 +27,14 @@ case class TypeSafeAccessToPersistentActorProvider() {
   )(
       implicit
       d: Decoder[Entity[V]]
-  ): Future[Option[Entity[V]]] = ??? // todo-right-now 2
-
+  ): Future[Option[Entity[V]]] = {
+    // we need to get a not type safe entity from the actor
+    // we need to turn that into a type safe one
+    val res: Future[Some[Entity[User]]] =
+      Future.successful(Some(TestUsers.aliceEntity_with_UUID0))
+    res.asInstanceOf[Future[Option[Entity[V]]]]
+    // ??? // todo-right-now  => fix this dummy implementation
+  }
 }
+
+
---
+++ modules/server/src/test/scala/app/server/httpServer/routes/post/routeLogicImpl/persistenceService/persistentActor/TypeSafeAccessToPersistentActorProviderTest.scala
@@ -0,0 +1,25 @@
+package app.server.httpServer.routes.post.routeLogicImpl.persistenceService.persistentActor
+
+import app.shared.initialization.testing.TestUsers
+import org.scalatest.FunSuite
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import io.circe.generic.auto._
+
+class TypeSafeAccessToPersistentActorProviderTest extends FunSuite {
+
+  // todo-right-now => make this test pass
+
+  test("testGetEntityWithVersion") {
+
+
+    val ae=TestUsers.aliceEntity_with_UUID0
+    val aer=ae.refToEntity
+    val tsap=TypeSafeAccessToPersistentActorProvider()
+    val res=tsap.getEntityWithVersion(aer)
+    val resa=Await.result(res, 1 second )
+    assert(res===resa)
+  }
+
+}
