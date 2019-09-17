4d514456e5e2114bb0de7254d4fcc07e622e3c54
Jozsef Hegedus <jhegedus@pm.me>
2019-09-18 01:44:10 +0300

    Tuesday, 17 September 2019 12.59.24:
    Trying to implement "login".
    
    --------------------------------------------------
    Tuesday, 17 September 2019 13.18.24 :
    
    Waiting for some help on how to implement
    "login" logic using the router.
    
    --------------------------------------------------
    Wednesday, 18 September 2019 0.43.35:
    
    Cleaning up the code a bit.
    --------------------------------------------------
    Wednesday, 18 September 2019 1.41.23:
    
    Added some "workaround text", as a "hack", the router
    is not really refreshing when its parents state changes.
    
    Which is weird/unexpected. ATM, I have no good ideas on
    how to implement the login page functionality without
    terrible hacks involving dreaded VARs crawling all over
    my skin... :) ... LOL ... I mean, in the code... which
    after coding 5 million hours... starts to feel like my
    skin :).

--- modules/client/src/main/scala/app/client/ui/components/router/mainPageComponents/LoginPageComp.scala
+++ modules/client/src/main/scala/app/client/ui/components/router/mainPageComponents/LoginPageComp.scala
@@ -19,7 +19,7 @@ object LoginPageComp {
       RouterWrapper.reRenderApp()
     }
   }
-  def isUserLoggedIn =State.isUserLoggedIn
+  def isUserLoggedIn = State.isUserLoggedIn
 
   class LoginPageBackend[Unit](
     $ : BackendScope[Unit, State.IsUserLoggedIn]) {
@@ -34,8 +34,43 @@ object LoginPageComp {
       s: State.IsUserLoggedIn
     ): VdomElement = {
 
+      val login =
+        if (s.yesOrNo)
+          <.div(
+            <.p(
+              "You are now logged in ! Yey !!! Welcome ! To the world, of tomorrow !!! :)"
+            ),
+            <.br,
+            <.p(
+              " In order to proceed to Nirvana, Zion, Valhalla, Enlightement, or call it whatever" +
+                " you wanna call it, " +
+                "please click  - on the top left - the " +
+                "Menu called 'Home', i.e. " +
+                "to see more functionality (to refresh the page). "
+            ),
+            <.br,
+            <.p(
+              "This is " +
+                "needed because the router cannot be re-freshed programmatically, " +
+                "or maybe it can but I have just yet not figured out how to do so. " +
+                "So this text here is a 'hack', so that 'users can use the app'. " +
+                "Welcome to da app :). I hope it will help you to learn a bit of " +
+                "scalajs-react, or Scala, or Scala.js, or whatever floats your boat " +
+                "or job, or both."
+            )
+          )
+        else {
+          import bootstrap4.TB.convertableToTagOfExtensionMethods
+          <.button.btn.btnPrimary(
+            "Press this button to log in.",
+            ^.onClick --> handleLoginButton()
+          )
+        }
+
       <.div(
-        <.main(C.justifyContentCenter ,C.container, ^.role := "container")(
+        <.main(C.justifyContentCenter,
+               C.container,
+               ^.role := "container")(
           <.div(C.jumbotron)(
             C.justifyContentCenter,
             C.justifyContentCenter,
@@ -44,14 +79,8 @@ object LoginPageComp {
             <.p(C.lead, "Login Status:"),
             <.br,
             s"${s.yesOrNo}",
-            <.br, {
-
-              import bootstrap4.TB.convertableToTagOfExtensionMethods
-              <.button.btn.btnPrimary(
-                "Press this button to log in.",
-                ^.onClick --> handleLoginButton()
-              )
-            }
+            <.br,
+            login
           )
         )
       )
