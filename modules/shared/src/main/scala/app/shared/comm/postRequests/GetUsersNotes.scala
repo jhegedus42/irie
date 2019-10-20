c61baebda463fe16af538e0089478fd34ee29d1f
Jozsef Hegedus <jhegedus@pm.me>
2019-10-20 19:45:18 +0300

    Started creating the Get User Notes route.

---
+++ modules/shared/src/main/scala/app/shared/comm/postRequests/GetUsersNotes.scala
@@ -0,0 +1,32 @@
+package app.shared.comm.postRequests
+
+import app.shared.comm.{PostRequest, ReadRequest, WriteRequest}
+import app.shared.entity.entityValue.EntityType
+import app.shared.entity.EntityWithRef
+import app.shared.entity.refs.{RefToEntityWithVersion}
+import io.circe.generic.JsonCodec
+
+object GetEntityReq { //todo-now CONTINUE HERE
+  import io.circe.generic.auto._
+  import io.circe.syntax._
+  import io.circe.generic.JsonCodec
+
+  @JsonCodec
+  case class Par[V <: EntityType[V]](
+    refToEntityWithVersion: RefToEntityWithVersion[V])
+      extends PostRequest.Parameter
+
+  @JsonCodec
+  case class Res[V <: EntityType[V]](
+    optionEntity: Option[EntityWithRef[V]])
+      extends PostRequest.Result
+}
+
+//@JsonCodec
+class GetEntityReq[V <: EntityType[V]]
+    extends PostRequest[ReadRequest] {
+  override type ParT     = GetEntityReq.Par[V]
+  override type ResT     = GetEntityReq.Res[V]
+  override type PayLoadT = V
+
+}
