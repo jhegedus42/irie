b7bbd9153eea932087edfbb18c5a680d3cf68bb1
Jozsef Hegedus <jhegedus@pm.me>
2019-11-08 04:51:36 +0200

    Sodium based caching implementation is in progress.

--- modules/client/src/main/scala/app/client/ui/caching/cache/sodiumCache/LatestEntitySodiumCache.scala
+++ modules/client/src/main/scala/app/client/ui/caching/cache/sodiumCache/LatestEntitySodiumCache.scala
@@ -2,6 +2,8 @@ package app.client.ui.caching.cache.sodiumCache
 
 import app.shared.entity.EntityWithRef
 import app.shared.entity.entityValue.EntityType
+import app.shared.utils.UUID_Utils.EntityIdentity
+import sodium._
 
 sealed trait EntityOperations[V<:EntityType[V]]{
 
@@ -20,12 +22,26 @@ case class UpdateEntity[V<:EntityType[V]](currentLatestEntityWithRef: EntityWith
 
 }
 
-trait LatestEntitySodiumCache[V<:EntityType[V]] {
+trait SodiumEntityCache[V<:EntityType[V]] {
 
   val entityUpdatedStream = ???
+  val entityInsertedStream = ???
+
+  val collectionChangedStream = ???
 
   def update(ref:EntityWithRef[V]) = {
       ???
   }
+
+  def insert(v:V) : Unit ={
+    val key = ???
+    val value = ???
+  }
+
+  type Key=EntityIdentity[V]
+  type Value=EntityWithRef[V]
+  val initMap=Map[Key,Value]()
+
+  val cellMapLoop = new CellLoop[Map[Key,Value]]()
 
 }
