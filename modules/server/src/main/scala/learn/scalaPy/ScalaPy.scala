14b28c8805c88e1fb39a8defd5f452791f1e5a4d
Jozsef Hegedus <jhegedus@pm.me>
2019-11-09 15:26:43 +0200

    heavy changes

--- modules/server/src/main/scala/learn/scalaPy/ScalaPy.scala
+++
@@ -1,9 +0,0 @@
-//package learn.scalaPy
-
-//import me.shadaj.scalapy.py
-
-//object ScalaPy extends App {
-//  val listLengthPython: py.Any = py.global.len(List(1, 2, 3))
-//  val listLength:       Int    = listLengthPython.as[Int]
-//  println(listLength)
-//}
