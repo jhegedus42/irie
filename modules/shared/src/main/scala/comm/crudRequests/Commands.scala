26d60f79f22c3be6b11feeadd8284fd70e3fa128
Jozsef Hegedus <jhegedus@pm.me>
2019-11-12 21:59:04 +0200

    now we have json decoding/encoding

--- modules/shared/src/main/scala/comm/crudRequests/Commands.scala
+++ modules/shared/src/main/scala/comm/crudRequests/Commands.scala
@@ -5,8 +5,12 @@ import dataStorage.stateHolder.UserMap
 import io.circe.syntax._
 import io.circe.generic.auto._
 import io.circe.generic.JsonCodec
+import io.circe._
 import dataStorage.UserRef
 import dataStorage.stateHolder.UserMap
+import io.circe.Decoder.Result
+import io.circe.parser._
+import jdk.nashorn.internal.parser.JSONParser
 
 sealed trait Command
 
@@ -18,9 +22,25 @@ case class GetAllEntityiesForUser(
   res: Option[UserMap])
     extends Command
 
+object GetAllEntityiesForUser {
+  implicit val jSONConvertable =
+    new JSONConvertable[GetAllEntityiesForUser] {
+      override def getJSON(v: GetAllEntityiesForUser): String =
+        v.asJson.toString()
 
-trait RouteName[V]{
-  def getRouteName:String
+      override def getObject(json: String): GetAllEntityiesForUser = {
+        val jsonParsed: Either[ParsingFailure, Json] = parse(json)
+        val res1:       Json                         = jsonParsed.toOption.get
+        val decoder = implicitly[Decoder[GetAllEntityiesForUser]]
+        val res2: Result[GetAllEntityiesForUser] =
+          decoder.decodeJson(res1)
+        res2.toOption.get
+      }
+    }
+}
+
+trait RouteName[V] {
+  def getRouteName: String
 }
 
 object RouteName {
@@ -29,11 +49,9 @@ object RouteName {
   }
 }
 
-trait JSONConvertable[V]{
-  def getJSON(v:V):String
-  def getObject(json:String) : V
+trait JSONConvertable[V] {
+  def getJSON(v:      V):      String
+  def getObject(json: String): V
 }
 
-object JSONConvertable {
-
-}
+object JSONConvertable {}
