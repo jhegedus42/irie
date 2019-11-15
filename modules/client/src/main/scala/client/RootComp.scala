6b94ff4070af8922288efecf1d974d871ebccaf6
Jozsef Hegedus <jhegedus@pm.me>
2019-11-15 05:16:53 +0200

    Some more slow progress.

--- modules/client/src/main/scala/client/RootComp.scala
+++ modules/client/src/main/scala/client/RootComp.scala
@@ -1,38 +1,9 @@
 package client
 import client.cache.{Cache, CacheMap, NormalizedStateHolder}
-import client.sodium.core.StreamSink
-import comm.crudRequests.{GetAllEntityiesForUser, JSONConvertable}
-import dataStorage.{RefToEntityOwningUser, User}
-import dataStorage.stateHolder.UserMap
-import io.circe.Json
-import org.scalajs.dom.document
-import org.scalajs.dom.ext.Ajax
-import org.scalajs.dom.raw.Element
-import sodium.components.{
-  SodiumButton,
-  SodiumPreformattedText,
-  SodiumRootComp
-}
-
-import scala.concurrent.{ExecutionContextExecutor, Future}
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSExport
-import scala.util.Try
-import japgolly.scalajs.react.{CtorType, _}
-import japgolly.scalajs.react.component.JsFn
-import japgolly.scalajs.react.component.ScalaFn.Unmounted
-import japgolly.scalajs.react.extra.router.RouterCtl
-import japgolly.scalajs.react.internal.Effect.Id
-import japgolly.scalajs.react.vdom.html_<^.{<, _}
-import org.scalajs.dom.html.Div
-import sodium.core._
-import testingData.TestEntitiesForUsers
-
-import client.cache.{Cache, CacheMap, NormalizedStateHolder}
 import client.sodium.components.SodiumPreformattedText
 import dataStorage.User
 import japgolly.scalajs.react.ScalaComponent
-import japgolly.scalajs.react.vdom.html_<^.<
+import japgolly.scalajs.react.vdom.html_<^.{<, _}
 
 import scala.concurrent.ExecutionContextExecutor
 
