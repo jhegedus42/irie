1f87316e4dcbf4f670cb9bd25a6e27040f5dff51
Jozsef Hegedus <jhegedus@pm.me>
2019-11-15 04:21:57 +0200

    abstracting Sodium Component Creation

---
+++ modules/client/src/main/scala/client/sodium/components/ComponentTemplate.scala
@@ -0,0 +1,38 @@
+package client.sodium.components
+
+import client.sodium.core.Stream
+import japgolly.scalajs.react.{BackendScope, Callback, ScalaComponent}
+import japgolly.scalajs.react.vdom.VdomElement
+import japgolly.scalajs.react.vdom.html_<^.<
+
+case class ComponentTemplate[V](
+  input:         Stream[V],
+  componentName: String,
+  initialState:  V,
+  renderer:      V => VdomElement) {
+
+  val comp = ScalaComponent
+    .builder[Unit](componentName)
+    .initialState(initialState)
+    .renderBackend[Backend]
+    .componentWillMount(f => {
+
+      Callback {
+        input.listen((x: V) => {
+          println(s"sodum label's cell is $x");
+          f.setState(x).runNow()
+        })
+      }
+
+    })
+    .build
+
+  class Backend($ : BackendScope[Unit, V]) {
+
+    def render(
+      unit:  Unit,
+      state: V
+    ): VdomElement = renderer(state)
+  }
+
+}
