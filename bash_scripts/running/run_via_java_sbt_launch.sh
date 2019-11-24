7c665655a4d64f2fd41cfe644fe1146102878775
Jozsef Hegedus <jhegedus@pm.me>
2019-11-24 20:32:25 +0100

    launch command - via java process call to sbt-launch.jar - seems to be working

--- bash_scripts/running/run_via_java_sbt_launch.sh
+++ bash_scripts/running/run_via_java_sbt_launch.sh
@@ -1,2 +1,4 @@
-/usr/bin/java -Xms1024m -Xmx1024m -XX:ReservedCodeCacheSize=128m -XX:MaxMetaspaceSize=256m -jar /usr/local/Cellar/sbt/1.2.8/libexec/bin/sbt-launch.jar ;clean;fastOptJS;server/run
+/usr/bin/java -Xms1024m -Xmx1024m -XX:ReservedCodeCacheSize=128m -XX:MaxMetaspaceSize=256m -jar /usr/local/Cellar/sbt/1.2.8/libexec/bin/sbt-launch.jar clean
+/usr/bin/java -Xms1024m -Xmx1024m -XX:ReservedCodeCacheSize=128m -XX:MaxMetaspaceSize=256m -jar /usr/local/Cellar/sbt/1.2.8/libexec/bin/sbt-launch.jar fastOptJS
+/usr/bin/java -Xms1024m -Xmx1024m -XX:ReservedCodeCacheSize=128m -XX:MaxMetaspaceSize=256m -jar /usr/local/Cellar/sbt/1.2.8/libexec/bin/sbt-launch.jar 'server/run 127.0.0.1'
 
