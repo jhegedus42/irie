b404b6e3a169f22f5281fd15425e710c42d0e53e
Jozsef Hegedus <jhegedus@pm.me>
2019-12-24 23:13:23 +0100

    Create todo list for Note CRUD.
    Took 9 minutes

--- modules/client/src/main/scala/client/Main.scala
+++ modules/client/src/main/scala/client/Main.scala
@@ -39,5 +39,20 @@ object Main extends js.JSApp {
     RootComp.getComp().renderIntoDOM(e)
 
     AJAXCalls.populateUserEntityCache()
+
+    // todonow 1 Note CRU
+    // todonow 1.1 display set of notes on client (test data)
+    // todonow 1.1.1 create cache for set of notes on client
+    // todonow 1.1.1.1 populate Note Entity Cache on client
+    // todonow 1.1.1.2 refactor Entity populater, abstract over
+    //  entities
+    // todonow 1.1.2 display list of notes for Alice
+    // todonow 1.2 select a note from the list (use the User Selector
+    //  Widget as template)
+    // todonow 1.3 display the content of the selected note
+    // todonow 1.4 create a widget that updates the text of the note
+    // todonow 1.5 create a widget that creates a new note
+    // todonow 1.6 create a widget that flags a note as "Moved to TrashCan"
+    //   todonow 1.7 add trashcan field to a Note
   }
 }
