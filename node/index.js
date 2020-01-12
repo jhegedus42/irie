0ad780325f4b79243313c3929373b1047e3563c9
Jozsef Hegedus <jhegedus@pm.me>
2020-01-12 16:09:56 +0100

    Cropping image in progress.
    Trying to get image cropping working in plain react.
    
    Took 32 minutes

--- node/index.js
+++ node/index.js
@@ -6,7 +6,7 @@ window.ReactDOM = require('react-dom');
 
 import React from 'react'
 
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 
 import sum from './otherFile.js';
 
@@ -16,9 +16,9 @@ import 'react-tabs/style/react-tabs.css';
 import TagsInput from 'react-tagsinput'
 import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
 
-window.TagsInput=TagsInput
+window.TagsInput = TagsInput
 
-const re= (
+const re = (
     <Tabs>
         <TabList>
             <Tab>Title 1</Tab>
@@ -34,21 +34,65 @@ const re= (
     </Tabs>
 );
 
-const f= () => {
+const f = () => {
     console.log('hello')
 
 }
 
+import ReactCrop from 'react-image-crop';
+
+import 'react-image-crop/dist/ReactCrop.css';
+
+const c = {
+    crop: {
+        unit: 'px', // default, can be 'px' or '%'
+        x: 130,
+        y: 50,
+        width: 200,
+        height: 200
+    }
+}
+
+class Bork {
+    static a = 'foo';
+    static b;
+
+    x = 'bar';
+    y;
+} // todo make this work
+
+class Demo extends React.PureComponent{
+
+
+    // state = {
+    //     src: null,
+    //     crop: {
+    //         unit: '%',
+    //         width: 30,
+    //         aspect: 16 / 9,
+    //     },
+    // };
+    // onCropChange = (crop, percentCrop) => {
+    //     // You could also use percentCrop:
+    //     // this.setState({ crop: percentCrop });
+    //     this.setState({ crop });
+    // };
+}
+
 var reactElement2 = (
-    <h1
-        className="abc"
-        style={{ textAlign: "center" }}
-        onClick={function() {
-            alert("click");
-        }}
-    >
-        Hello, world 42!
-    </h1>
+    <div>
+        <h1
+            className="abc"
+            style={{textAlign: "center"}}
+            onClick={function () {
+                alert("click");
+            }}
+        >
+            Hello, world 42!
+        </h1>
+
+        <ReactCrop src={"6a7e6ec8-daf8-4773-b977-76d6e27e5591.jpeg"} onChange={newCrop => console.log(newCrop)}/>
+    </div>
 );
 
 // import Croppr from 'croppr';
@@ -78,5 +122,6 @@ var anotherElement = React.createElement(
 
 var renderTarget = document.getElementById("testComp");
 
-window.ReactDOM.render(re, renderTarget);
+window.ReactDOM.render(reactElement2, renderTarget);
+
 
