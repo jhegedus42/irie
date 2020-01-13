0744770708ea4bf4e8e306a4b573dfdfa45ebfee
Jozsef Hegedus <jhegedus@pm.me>
2020-01-13 16:18:27 +0100

    Note folder is in progress.
    Took 8 minutes

--- modules/client/src/main/scala/client/ui/compositeWidgets/noteFolder/NoteFolderWidget.scala
+++ modules/client/src/main/scala/client/ui/compositeWidgets/noteFolder/NoteFolderWidget.scala
@@ -1,6 +1,8 @@
 package client.ui.compositeWidgets.noteFolder
 
 import client.cache.Cache
+import client.sodium.core.Cell
+import client.ui.atomicWidgets.show.text.CellPreformattedText
 import client.ui.compositeWidgets.general.EntitySelectorWidget
 import japgolly.scalajs.react.ScalaComponent
 import japgolly.scalajs.react.vdom.html_<^.{<, VdomElement}
@@ -19,9 +21,19 @@ case class NoteFolderWidget() {
   implicit lazy val noteFolderCache: Cache[NoteFolder] =
     Cache.noteFolderCache
 
-  val selector = EntitySelectorWidget[NoteFolder]( { x: NoteFolder =>
+  val selector = EntitySelectorWidget[NoteFolder]({ x: NoteFolder =>
     x.name
   })
+
+  val selectedNoteFoldersName: Cell[String] =
+    selector.selectedEntity.map(
+      _.map(_.versionedEntityValue.valueWithoutVersion.name)
+        .getOrElse("")
+    )
+
+  val selectedNoteFoldersNameComp = CellPreformattedText(
+    selectedNoteFoldersName
+  )
 
   def getComp = {
 
@@ -31,6 +43,7 @@ case class NoteFolderWidget() {
         <.h2("Note Folders"),
         <.br,
         selector.selectorTable.comp(),
+        selectedNoteFoldersNameComp.comp(),
         <.hr,
         <.br
       )
@@ -49,6 +62,5 @@ case class NoteFolderWidget() {
   // todo-now 1: display note folder of a selected note
   // todo-now 2: set note folder of a selected note to
   //             one of the possible note folders
-
 
 }
