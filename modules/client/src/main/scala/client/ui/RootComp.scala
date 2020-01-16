6df3ed8ff240641095117201e85e6c322ffacd25
Jozsef Hegedus <jhegedus@pm.me>
2020-01-16 22:02:14 +0100

    1. Implementing ordering for note folders.
    1.1 Implementing batch entity update.
    
    Took 11 minutes

--- modules/client/src/main/scala/client/ui/RootComp.scala
+++ modules/client/src/main/scala/client/ui/RootComp.scala
@@ -9,7 +9,12 @@ import client.ui.compositeWidgets.archive.CounterExampleWidget
 import client.ui.compositeWidgets.image.{ImageList, ImagesWidget}
 import client.ui.compositeWidgets.note.NotesWidget
 import client.ui.compositeWidgets.noteFolder.NoteFolderWidget
-import client.ui.wrappedReact.{Crop, ReactCrop, ReactCropWrapped, TagsInput}
+import client.ui.wrappedReact.{
+  Crop,
+  ReactCrop,
+  ReactCropWrapped,
+  TagsInput
+}
 
 object RootComp {
 
@@ -28,7 +33,9 @@ object RootComp {
             <.br,
             TagsInput(value    = Seq("foo", "bar42"),
                       onChange = TagsInput.handlerCore(_)),
-            ReactCropWrapped.comp("6a7e6ec8-daf8-4773-b977-76d6e27e5591.jpeg" ),
+            ReactCropWrapped.comp(
+              "6a7e6ec8-daf8-4773-b977-76d6e27e5591.jpeg"
+            ),
             NoteFolderWidget().getComp()
           )
         )
@@ -43,8 +50,6 @@ object RootComp {
 
     rootComp
   }
-
-
- // todo-now - ListOfNotes
+  // todo-now - ListOfNotes
 
 }
---
+++ modules/client/src/main/scala/client/ui/compositeWidgets/note/NoteFolderUpdaterWidget.scala
@@ -0,0 +1,138 @@
+package client.ui.compositeWidgets.note
+
+import client.cache.Cache
+import client.sodium.core.Cell
+import client.ui.atomicWidgets.input.SButton
+import client.ui.atomicWidgets.show.text.CellPreformattedText
+import client.ui.compositeWidgets.general.{CellOptionDisplayerWidget, EntitySelectorWidget, EntityUpdaterButton}
+import japgolly.scalajs.react.ScalaComponent
+import japgolly.scalajs.react.vdom.html_<^.{<, VdomElement}
+import shared.dataStorage.{Note, NoteFolder, Ref, TypedReferencedValue, VersionedValue}
+import japgolly.scalajs.react.vdom.html_<^.{<, VdomElement, _}
+
+import scala.concurrent.ExecutionContextExecutor
+
+case class NoteFolderUpdaterWidget(
+  selectedNote: Cell[Option[TypedReferencedValue[Note]]]) {
+
+  implicit def executionContext: ExecutionContextExecutor =
+    scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+
+  implicit lazy val noteFolderCache: Cache[NoteFolder] =
+    Cache.noteFolderCache
+
+  val noteFolderSelectorWidget = EntitySelectorWidget[NoteFolder]({
+    x: NoteFolder =>
+      x.name
+  })
+
+//  val selectedNoteDisplayer =
+//    CellOptionDisplayerWidget[Note](
+//      selectedNote.map(
+//        _.map(_.versionedEntityValue.valueWithoutVersion)
+//      ), { n: Note =>
+//        <.div(
+//          s"Selected Note's title: ${n.title}",
+//          <.br,
+//          (s"Selected Note's NoteFolder's Ref: ${n.locationInNoteFolderOpt}")
+//        )
+//      }
+//    )
+
+  lazy val selectedNoteFolderDisplayer =
+    CellOptionDisplayerWidget[NoteFolder](
+      noteFolderSelectorWidget.selectedEntity.map(
+        _.map(_.versionedEntityValue.valueWithoutVersion)
+      ), { nf: NoteFolder =>
+        <.div(
+          <.br,
+          s"Selected NoteFolder's name:",
+          nf.name,
+          <.br
+        )
+      }
+    )
+
+//  lazy val selectedNotesNoteFolder: Cell[Option[NoteFolder]] = {
+//
+//    val sn: Cell[Option[Ref[NoteFolder]]] =
+//      selectedNote.map((x: Option[TypedReferencedValue[Note]]) => {
+//        x.flatMap(
+//          (z: TypedReferencedValue[Note]) => {
+//            val res1: Option[Ref[NoteFolder]] =
+//              z.versionedEntityValue.valueWithoutVersion.locationInNoteFolderOpt.map(_.nf)
+//            res1
+//          }
+//        )
+//      })
+//
+//    val res: Cell[Option[TypedReferencedValue[NoteFolder]]] =
+//      Cache.resolveRef(sn)
+//    res.map(_.map(_.versionedEntityValue.valueWithoutVersion))
+//  }
+
+//  lazy val selectedNotesNoteFolderDisplayer =
+//    CellOptionDisplayerWidget[NoteFolder](
+//      selectedNotesNoteFolder, { nf: NoteFolder =>
+//        <.div(
+//          <.br,
+//          s"Selected Note's NoteFolder's name:",
+//          nf.name,
+//          <.br
+//        )
+//      }
+//    )
+
+//  lazy val entityUpdaterButton
+//    : SButton = {
+//
+//    def setter(
+//      nf: Option[Ref[NoteFolder]],
+//      n:  Note
+//    ): Note = {
+////      n.copy(folderR = nf)
+//      ??? // todo-now
+//    }
+//
+//    val newValue: Cell[Option[Ref[NoteFolder]]] =
+//      noteFolderSelectorWidget.selectedEntity.map(_.map(_.ref))
+//
+//    EntityUpdaterButton[Note, Option[Ref[NoteFolder]]](
+//      selectedNote,
+//      Cache.noteCache,
+//      setter,
+//      newValue,
+//      "update"
+//    )
+//
+//    ???
+//  }
+
+  def getComp = {
+
+    def render: Unit => VdomElement = { _ =>
+      <.div(
+        <.hr,
+        <.h2("Note Folder Updater"),
+        <.br,
+        noteFolderSelectorWidget.selectorTable.comp(),
+//        selectedNoteFolderDisplayer.displayer(),
+//        selectedNoteDisplayer.displayer(),
+//        selectedNotesNoteFolderDisplayer.displayer(),
+//        entityUpdaterButton.comp(),
+//        <.hr,
+        <.br
+      )
+    }
+
+    val rootComp =
+      ScalaComponent
+        .builder[Unit]("Hello")
+        .render_P(render)
+        .build
+
+    rootComp
+
+  }
+
+}
