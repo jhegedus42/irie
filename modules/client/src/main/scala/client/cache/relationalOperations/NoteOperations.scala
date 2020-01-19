db8b093ee59256240b562d177908c24cd4b9d2c2
Jozsef Hegedus <jhegedus@pm.me>
2020-01-19 18:28:59 +0100

    Support for adding images (with rect-s) to notes.
    Took 14 minutes

--- modules/client/src/main/scala/client/cache/relationalOperations/NoteOperations.scala
+++ modules/client/src/main/scala/client/cache/relationalOperations/NoteOperations.scala
@@ -2,7 +2,12 @@ package client.cache.relationalOperations
 
 import client.cache.relationalOperations.CellOptionMonad.CellOption
 import client.sodium.core.Cell
-import shared.dataStorage.{ImageWithQue, Note, TypedReferencedValue}
+import shared.dataStorage.{
+  ImageWithQue,
+  Note,
+  Ref,
+  TypedReferencedValue
+}
 
 object NoteOperations {
 
@@ -10,22 +15,25 @@ object NoteOperations {
     coImg: CellOption[TypedReferencedValue[ImageWithQue]]
   ): CellOption[Set[TypedReferencedValue[Note]]] = {
 
+    val criteria
+      : CellOption[TypedReferencedValue[Note] => Boolean] = {
 
+      val res: CellOption[TypedReferencedValue[Note] => Boolean] =
+        for {
+          img <- coImg
+          f = (n: TypedReferencedValue[Note]) => {
+            img.versionedEntityValue.valueWithoutVersion.referenceToNote
+              .map((xn: Ref[Note]) => xn == n.ref)
+          }.getOrElse(false)
 
-    val criteria : CellOption[Note=>Boolean] = {
-
-      for {
-        img <-coImg
-//        f = (n:Note) => n.
-      } yield 2
-
-      ???
+        } yield f
+      res
     }
 
     val res =
-      RelationalOperations.coGetAllEntitiesWithFilter[Note](???)
+      RelationalOperations.coGetAllEntitiesWithFilter[Note](criteria)
 
-   res
+    res
   }
 
 }
--- modules/client/src/main/scala/client/ui/compositeWidgets/specific/image/ImageDisplayerWidget.scala
+++ modules/client/src/main/scala/client/ui/compositeWidgets/specific/image/ImageDisplayerWidget.scala
@@ -3,31 +3,64 @@ package client.ui.compositeWidgets.specific.image
 import java.net.FileNameMap
 
 import client.cache.Cache
+import client.cache.relationalOperations.{
+  CellOptionMonad,
+  NoteOperations
+}
 import client.sodium.core.Cell
 import client.ui.compositeWidgets.general.CellOptionDisplayerWidget
-import shared.dataStorage.{ImageWithQue, TypedReferencedValue, Value}
+import shared.dataStorage.{
+  ImageWithQue,
+  Note,
+  TypedReferencedValue,
+  Value
+}
 import japgolly.scalajs.react.vdom.html_<^.{<, VdomElement, _}
 
 case class ImageDisplayerWidget(
-  img: Cell[Option[TypedReferencedValue[ImageWithQue]]]
-) {
+  img: Cell[Option[TypedReferencedValue[ImageWithQue]]]) {
 
   lazy val cellOptionImage: Cell[Option[ImageWithQue]] =
     img.map(_.map(_.versionedEntityValue.valueWithoutVersion))
 
-
-  def getImg(fileNameOpt:Option[String]) : VdomElement = {
-    if(fileNameOpt.isDefined){
-      val fn=fileNameOpt.head
+  def getImg(fileNameOpt: Option[String]): VdomElement = {
+    if (fileNameOpt.isDefined) {
+      val fn = fileNameOpt.head
       <.div(
         s"File name :$fn.",
-        <.img(^.src := s"$fn", ^.alt:="image", ^.width:="100%"),
+        <.img(^.src := s"$fn", ^.alt := "image", ^.width := "100%"),
         <.br
       )
+    } else {
+      <.div(
+        "Image file has not been uploaded for this Image Entity yet."
+      )
     }
-    else{
-      <.div("Image file has not been uploaded for this Image Entity yet.")
-    }
+  }
+
+  lazy val notesBelongingToThisImage = {
+
+    lazy val res
+      : CellOptionMonad.CellOption[Set[TypedReferencedValue[Note]]] =
+      NoteOperations.getNotesForAnImage(???)
+
+    CellOptionDisplayerWidget[Set[TypedReferencedValue[Note]]](
+      res.co, { x: Set[TypedReferencedValue[Note]] =>
+
+
+        <.div(
+          <.hr,
+          <.br,
+          "The following notes refer to this Image:",
+
+          // todo - write a list displayer
+
+          <.br,
+          <.br,
+          <.hr
+        )
+      }
+    )
   }
 
   lazy val imageDisplayer = {
@@ -52,6 +85,5 @@ case class ImageDisplayerWidget(
       }
     ).displayer
   }
-
 
 }
