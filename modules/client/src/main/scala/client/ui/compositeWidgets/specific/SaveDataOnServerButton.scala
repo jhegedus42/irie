8db31eb8565808d3481e9ca1ca0fd96b8f1c5280
Jozsef Hegedus <jhegedus@pm.me>
2020-01-31 23:11:02 +0100

    Password protected updating works.
    Took 20 minutes

--- modules/client/src/main/scala/client/ui/compositeWidgets/specific/SaveDataOnServerButton.scala
+++ modules/client/src/main/scala/client/ui/compositeWidgets/specific/SaveDataOnServerButton.scala
@@ -1,24 +1,32 @@
 package client.ui.compositeWidgets.specific
 
 import client.cache.AJAXCalls
-import client.ui.atomicWidgets.input.SButton
-
+import client.ui.atomicWidgets.input.{SButton, STextArea}
 import japgolly.scalajs.react.vdom.html_<^.{<, _}
 import japgolly.scalajs.react.vdom.html_<^.{<, VdomElement}
 
+case class SaveDataOnServerButton() {
 
-case class SaveDataOnServerButton(){
+  val pwdField = STextArea("")
 
-  lazy val textButton = ???
-  lazy val btn= SButton("Save Data On Server",Some({
-    () => {
-      println("we gonna save the data on the server !")
-      AJAXCalls.saveDataOnServer("test2")
-      println("AJAX call for saving data on the server has been sent.")
-    }
-  }))
+  lazy val btn = SButton(
+    "Save Data On Server",
+    Some({ () =>
+      {
+        println("we gonna save the data on the server !")
+        AJAXCalls.saveDataOnServer(pwdField.cell.sample())
+        println(
+          "AJAX call for saving data on the server has been sent."
+        )
+      }
+    })
+  )
 
-  lazy val vdom= <.div(
-   btn.comp()
+  lazy val vdom = <.div(
+    "Password",
+    <.br,
+    pwdField.comp(),
+    <.br,
+    btn.comp()
   )
 }
